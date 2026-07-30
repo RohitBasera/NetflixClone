f0e0cd20e4beecdf12e34c572d8638beaa6dfa77
Rohit Basera <rudrsingh2004@gmail.com>
2026-07-30 05:26:22 -0700

    Initial Netflix Clone setup

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCards.css'
+
+const TitleCards = () => {
+  return (
+    <div className='tilecards'>
+      
+    </div>
+  )
+}
+
+export default TitleCards
