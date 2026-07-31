ef0f2f4e58de0bc994e6c72babd93fe4bfba40d4
Rohit Basera <rudrsingh2004@gmail.com>
2026-07-31 09:28:15 -0700

    Updated navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
-import 'Navbar.css'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className='navbar__left'>
+        <img src={logo} alt=""/>
+        <ul>
+          <li>Home</li>
+          <li>TV SHOWS </li>
+          <li>MOVIES</li> 
+          <li>NEW & POPULAR</li>  
+          <li>My List</li>
+          <li>Browse by languages</li>         
+        </ul>
+      </div>
+      <div className='navbar__right'>
+        <img src={search_icon} alt="" className="icon"/>
+        <p>Children</p>
+        <img src={bell_icon} alt="" className="icon"/>
+        <div className='navbar-profile'>
+        <img src={profile_img} alt="" className="profile"/>
+        <img src={caret_icon} alt="" className="caret"/>
+        </div>
+      </div>
     </div>
   )
 }
