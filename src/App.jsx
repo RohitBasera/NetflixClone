468cc11ed6bfd0cf51a38174f50d4a4c1475920b
Rohit Basera <rudrsingh2004@gmail.com>
2026-08-04 07:55:27 -0700

    added navbar and css updates,completed home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import Home from './pages/Home/Home';
-import Navbar from './components/Navbar/Navbar';
+import React from 'react'
+import Home from './pages/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+import{Route,Routes} from 'react-router-dom'
+import Login from './pages/Login/Login'
 
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />  
+      </Routes>
       
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,8 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
   return (
@@ -17,11 +19,18 @@ const Home = () => {
           <p>Discovering his ties to seceret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an imortal enemy.</p>
           <div className="hero-btns">
             <button className='btn'><img src={play_icon} alt=""/> Play</button>
-            <button className='btn'><img src={info_icon} alt=""/> More Info</button>
+            <button className='btn dark-btn'><img src={info_icon} alt=""/> More Info</button>
            </div>
+           <TitleCards/>
            </div>
       </div>
-      
+      <div className="more-cards">
+      <TitleCards title={"Blockbuster Movies"}/> 
+      <TitleCards title={"Only on Netflix"}/> 
+      <TitleCards title={"Upcoming"}/> 
+      <TitleCards title={"Handpicked For You"}/> 
+      </div>
+      <Footer/>
     </div>
   )
 }
