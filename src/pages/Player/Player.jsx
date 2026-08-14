0fdde4cbcd42fb6898dfa0654e568aacec8a7d85
Rohit Basera <rudrsingh2004@gmail.com>
2026-08-14 07:06:30 -0700

    Player page

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './player.css'
-
+import back_arrow_icon from '../../assets/back_arrow.png'
 const Player = () => {
   return (
     <div className='player'>
-      
+      <img src={back_arrow_icon} alt="Back" />
+      <iframe width='90%' height='90%' src="https://www.youtube.com/embed/hlfz1ep8IL4" title="trailer" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
   )
 }
